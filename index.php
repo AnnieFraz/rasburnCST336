f01357b352342b5451cf9efd61832d494bc9782b
Anna Rasburn <annarasburn97@gmail.com>
2017-09-19 19:42:03 +0000

    small change to index.php file - 19/9

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -6,12 +7,12 @@
     </head>
     <body>
     <a href="labs">Labs</a>
-    <td><a href="Labs/1">Lab 1</a></td>
-    <td><a href="Labs/2">Lab 2</a></td>
-    <td><a href="Labs/3">Lab 3</a></td>
+    <a href="Labs/1">Lab 1</a>
+    <a href="Labs/2">Lab 2</a>
+    <a href="Labs/3">Lab 3</a>
     <a href="Homeworks">Homework</a>
-    <td><a href="Homeworks/1">Lab 1</a></td>
-    <td><a href="Homeworks/2">Lab 2</a></td>
+    <a href="Homeworks/1">Lab 1</a>
+    <a href="Homeworks/2">Lab 2</a>
     </body>
 </html>
-?>
+
