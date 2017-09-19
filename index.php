dca2ec7c9b4a5a1b94ace93a5808b14e72b58ab9
Anna Rasburn <annarasburn97@gmail.com>
2017-09-19 19:34:47 +0000

    Updated index file with php - 19/9

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Anna Rasburn CST 336 </title>
+    </head>
+    <body>
+    <a href="labs">Labs</a>
+    <td><a href="Labs/1">Lab 1</a></td>
+    <td><a href="Labs/2">Lab 2</a></td>
+    <td><a href="Labs/3">Lab 3</a></td>
+    <a href="Homeworks">Homework</a>
+    <td><a href="Homeworks/1">Lab 1</a></td>
+    <td><a href="Homeworks/2">Lab 2</a></td>
+    </body>
+</html>
+?>
