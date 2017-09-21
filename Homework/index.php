3fa094381cf9a188678233797da775355bf02180
Anna Rasburn <annarasburn97@gmail.com>
2017-09-21 21:32:25 +0000

    updated hwk1

---
+++ Homework/index.php
@@ -0,0 +1,12 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Anna Rasburn CST 336 </title>
+    </head>
+    <body>
+    <a href="hw1">Homework 1</a>
+    <a href="hw2">Homework 2</a>
+    </body>
+</html>
--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <a href="Labs/1">Lab 1</a>
     <a href="Labs/2">Lab 2</a>
     <a href="Labs/3">Lab 3</a>
-    <a href="Homework">Homework</a>
+    <a href="Homework/index.php">Homework</a>
     <a href="Homework/hw1">Homework 1</a>
     <a href="Homework/hw2">Homework 2</a>
     </body>
